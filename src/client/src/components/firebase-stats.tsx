ba6c2519aad3867baad96051a189c9e6c027e6da
Carles Capellas <capellas.carles@gmail.com>
2022-01-14 10:03:19 +0100

    mock firebase stats network call

--- src/client/src/components/firebase-stats.tsx
+++ src/client/src/components/firebase-stats.tsx
@@ -1,37 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FirebaseStats as IFirebaseStats } from '../../../shared/types';
+import { mockFirebaseStats } from '../mock-firebase-stats';
 import { CustomTable } from './custom-table';
 
 interface FirebaseStatsProps {
     authToken: string;
 }
 
-export const FirebaseStats: React.FC<FirebaseStatsProps> = (props) => {
-    const [errorMessage, setErrorMessage] = useState<string>();
-    const [stats, setStats] = useState<Partial<IFirebaseStats>>({});
+export const FirebaseStats: React.FC<FirebaseStatsProps> = () => {
+    // const [errorMessage, setErrorMessage] = useState<string>();
+    const [stats /*, setStats*/] = useState<Partial<IFirebaseStats>>(mockFirebaseStats);
 
-    useEffect(() => {
-        fetch('/api/firebase-stats', {
-            headers: {
-                Authorization: props.authToken
-            },
-            method: 'GET'
-        })
-            .then((response) => {
-                if (response.ok) {
-                    response.json().then(setStats);
-                } else {
-                    response.json().then((error) => setErrorMessage(error.message));
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-                setErrorMessage(error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch('/api/firebase-stats', {
+    //         headers: {
+    //             Authorization: props.authToken
+    //         },
+    //         method: 'GET'
+    //     })
+    //         .then((response) => {
+    //             if (response.ok) {
+    //                 response.json().then(setStats);
+    //             } else {
+    //                 response.json().then((error) => setErrorMessage(error.message));
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //             setErrorMessage(error);
+    //         });
+    // }, []);
     return (
         <div>
-            <p>{errorMessage}</p>
+            {/* <p>{errorMessage}</p> */}
             <p>Notifiable users: {stats.notifiableUsers || '-'}</p>
             <p>Uploaded receipts: {stats.uploadedReceipts || '-'}</p>
             <p>Users without receipts: {stats.usersWithoutReceipt || '-'}</p>
---
+++ src/client/src/mock-firebase-stats.ts
@@ -0,0 +1,221 @@
+export const mockFirebaseStats = {
+    notifiableUsers: 189,
+    uploadedReceipts: 149,
+    usersWithoutReceipt: 126,
+    receiptsRanking: [
+        { userEmail: 'mxxxxxxxxxxy@yyy.com', userId: 'cBVHX6HV56RDP5W94owP3BiZcUo1', receipts: 22 },
+        {
+            userEmail: 'oxxxxxxxxxxm@yyy.com',
+            userId: 'yiF5GLJypyOzWFfYDwktlUgDMMW2',
+            receipts: 16
+        },
+        { userEmail: 'oxxxxxxxxxxa@yyy.es', userId: 'vYLK6dF3cLZtIh8xv3hkbrckMJv2', receipts: 15 },
+        { userEmail: 'jxxxxxxxxxxr@yyy.com', userId: 'rxfuI0gCpVYhpHej1w0P31cAFe22', receipts: 5 },
+        {
+            userEmail: 'axxxxxxxxxxa@yyy.com',
+            userId: 'r3rSfEzZDFP6nP5bflMqLSgzIu03',
+            receipts: 4
+        },
+        { userEmail: 'bxxxxxxxxxxa@yyy.com', userId: 'RSPogCEMGaTs6PgNIzPoYcXmvvs1', receipts: 4 },
+        { userEmail: 'exxxxxxxxxx8@yyy.com', userId: 'ktt5bc3LgiWH94KottjmM64SeL52', receipts: 4 },
+        {
+            userEmail: 'mxxxxxxxxxxa@yyy.com',
+            userId: '0XVm3kR4EdMuzFtQKv1wfymo02u1',
+            receipts: 4
+        },
+        {
+            userEmail: 'pxxxxxxxxxxa@yyy.com',
+            userId: '42cLBTMK0yVmSVm3wEFDbZeiNVp2',
+            receipts: 4
+        },
+        {
+            userEmail: 'axxxxxxxxxx1@yyy.com',
+            userId: 'hUpRkbEKZhNPDtPuRHvLpJcDO1Y2',
+            receipts: 3
+        },
+        {
+            userEmail: 'cxxxxxxxxxxs@yyy.com',
+            userId: '3ayUIFRc4YSrchsoQNBIOFumOp72',
+            receipts: 3
+        },
+        { userEmail: 'nxxxxxxxxxx9@yyy.com', userId: '2amh68JGitcMsOm5mlxTY6qbOGQ2', receipts: 3 },
+        { userEmail: 'sxxxxxxxxxxo@yyy.com', userId: 'AoMtFt7dK3bqad7ZozQL2M9uD7G3', receipts: 3 },
+        { userEmail: 'vxxxxxxxxxx4@yyy.com', userId: 'ELxHiSZvYGeBJxEVMq1ZNwxO19i1', receipts: 3 },
+        {
+            userEmail: 'exxxxxxxxxxr@yyy.com',
+            userId: 'VqIFMWTdkaZRGXR3Z5dVT9nl9gr1',
+            receipts: 2
+        },
+        { userEmail: 'exxxxxxxxxxc@yyy.com', userId: 'DdiS8bEaq2gluxYeapZ5hBp3X6v2', receipts: 2 },
+        { userEmail: 'jxxxxxxxxxxs@yyy.com', userId: 'zI8wXvcB8dfQTJK3kvPtqclgX4R2', receipts: 2 },
+        { userEmail: 'pxxxxxxxxxxs@yyy.com', userId: 'icZuQOXjTnRzgxM3uFIQBSsR9KD3', receipts: 2 },
+        {
+            userEmail: 'qxxxxxxxxxxh@yyy.com',
+            userId: 'HflREiaLwCO7syGnnLyK0MO15Tt2',
+            receipts: 2
+        },
+        {
+            userEmail: 'rxxxxxxxxxxa@yyy.com',
+            userId: 'eajg0S3DxlXrMa6za79r6NLFitw2',
+            receipts: 2
+        },
+        {
+            userEmail: 'sxxxxxxxxxx7@yyy.com',
+            userId: 'eT9leLc4LOTG3H1Z2IraQ5tZ7F02',
+            receipts: 2
+        },
+        {
+            userEmail: '2xxxxxxxxxxc@yyy.com',
+            userId: 'UI9XpAoJuRh3aVpCkyrOPH7lYbj1',
+            receipts: 1
+        },
+        {
+            userEmail: 'axxxxxxxxxxn@yyy.com',
+            userId: 'gpmlviKSYqfs3lfDEXVLowKDXRd2',
+            receipts: 1
+        },
+        {
+            userEmail: 'axxxxxxxxxxu@yyy.com',
+            userId: 'M9OM4EY5zObo6BiKEdZzjB2QHIW2',
+            receipts: 1
+        },
+        {
+            userEmail: 'axxxxxxxxxx0@yyy.com',
+            userId: 'V6RSthoFI0WmIhKfP5Fn2jToGpm2',
+            receipts: 1
+        },
+        { userEmail: 'axxxxxxxxxxa@yyy.com', userId: '3HmfLB70sbV5UPPwoNWdsIfaVeO2', receipts: 1 },
+        { userEmail: 'axxxxxxxxxxa@yyy.com', userId: 'Q1imjWGTxhM7OjyAeAOq0ebP1Lk2', receipts: 1 },
+        {
+            userEmail: 'cxxxxxxxxxx6@yyy.com',
+            userId: 'V5oNsNPAb1SPnwwbQCLhJv5D85D2',
+            receipts: 1
+        },
+        { userEmail: 'cxxxxxxxxxx3@yyy.com', userId: 'RNam0ktYmlckXVdzapx4CsNzRyu1', receipts: 1 },
+        {
+            userEmail: 'cxxxxxxxxxxe@yyy.com',
+            userId: '4MXHvYWVvyPf4MBhLXJLUvl5wsL2',
+            receipts: 1
+        },
+        { userEmail: 'cxxxxxxxxxxr@yyy.com', userId: 'qUZMsytjeRXaqoIx4d5dHFdkSZF3', receipts: 1 },
+        {
+            userEmail: 'cxxxxxxxxxxo@yyy.com',
+            userId: 'ZywdpO8VpOeEIbgr8NJ2xy60ZMw1',
+            receipts: 1
+        },
+        { userEmail: 'dxxxxxxxxxxm@yyy.com', userId: 'wZuCz9h0dyMxZgT7xMfeeeJhZQF2', receipts: 1 },
+        { userEmail: 'dxxxxxxxxxxa@yyy.com', userId: 'W3Uetc6g8mOzOGoTT82J95kTCRt2', receipts: 1 },
+        {
+            userEmail: 'dxxxxxxxxxxl@yyy.com',
+            userId: 'Icbvz6NMb9bHXhUY2MZz9BWIBKk2',
+            receipts: 1
+        },
+        { userEmail: 'dxxxxxxxxxx3@yyy.cat', userId: 'EIwcZdeY3eZKACii3k6eR4bOS9g1', receipts: 1 },
+        { userEmail: 'dxxxxxxxxxxh@yyy.com', userId: '8uEMZLjflPPuSkWgzLiCU89qlio2', receipts: 1 },
+        { userEmail: 'exxxxxxxxxx1@yyy.com', userId: 'GQjCzrrP26OEhEtDSwUKKe2znOO2', receipts: 1 },
+        {
+            userEmail: 'gxxxxxxxxxx7@yyy.com',
+            userId: '4Xu7vavggTeTUe6qVzv0JOWihS72',
+            receipts: 1
+        },
+        { userEmail: 'gxxxxxxxxxxs@yyy.com', userId: 'dNrhrsCIeTbYU0BWndifUFYWooE3', receipts: 1 },
+        {
+            userEmail: 'ixxxxxxxxxxo@yyy.com',
+            userId: 'WgFZihXtdeY7zgXII0NyPe8YLpB2',
+            receipts: 1
+        },
+        {
+            userEmail: 'ixxxxxxxxxxo@yyy.com',
+            userId: 'lSkTkK5b0VWvURJuRUypRjOaiqg1',
+            receipts: 1
+        },
+        {
+            userEmail: 'ixxxxxxxxxxa@yyy.com',
+            userId: 'LBg8NruTDaTC4gDgZ6dnmVQanE93',
+            receipts: 1
+        },
+        { userEmail: 'jxxxxxxxxxx7@yyy.com', userId: 'uLGnD7OvlGSyLgbj5HRJxF7AHWj1', receipts: 1 },
+        {
+            userEmail: 'kxxxxxxxxxxk@yyy.com',
+            userId: 'pXC1Chjf7tZzfu6oB0QR7ZynEPi1',
+            receipts: 1
+        },
+        {
+            userEmail: 'mxxxxxxxxxxa@yyy.com',
+            userId: 'IPTZjbRklRT6N9jswSHeoAEHv8l2',
+            receipts: 1
+        },
+        { userEmail: 'mxxxxxxxxxx6@yyy.com', userId: '21bwhCVtwYQcQz9Swv6L3iQyrcF3', receipts: 1 },
+        {
+            userEmail: 'nxxxxxxxxxxa@yyy.com',
+            userId: 'pfuGBigL4OWqryKaDWDOpvlkZCE2',
+            receipts: 1
+        },
+        { userEmail: 'nxxxxxxxxxxt@yyy.com', userId: 'dB6xdbsBQ2f6w9VlM1yCKmgIDOL2', receipts: 1 },
+        { userEmail: 'nxxxxxxxxxxr@yyy.com', userId: '53q5ume2AoOXWYZ0bdWbXeKFTBy2', receipts: 1 },
+        {
+            userEmail: 'nxxxxxxxxxx3@yyy.com',
+            userId: 'DyC6CvZMmXP4cYRr9G1npIWWmrt2',
+            receipts: 1
+        },
+        { userEmail: 'nxxxxxxxxxxo@yyy.com', userId: 'qQou0AjOZfgHiki4xl3wpibsjv82', receipts: 1 },
+        {
+            userEmail: 'pxxxxxxxxxxa@yyy.com',
+            userId: 'jD8FisfHzDUaGGtwvYHSiHXandO2',
+            receipts: 1
+        },
+        {
+            userEmail: 'qxxxxxxxxxx5@yyy.com',
+            userId: '8kgBXJxykrQA8GzGVJVdAXrDsUz1',
+            receipts: 1
+        },
+        { userEmail: 'rxxxxxxxxxx0@yyy.com', userId: '45M8DdpfP1bfdq2IkRMuTG4sg9u2', receipts: 1 },
+        {
+            userEmail: 'rxxxxxxxxxx3@yyy.com',
+            userId: 'Lne0H5GKbQPg9HbmhvwczVafWTF3',
+            receipts: 1
+        },
+        { userEmail: 'sxxxxxxxxxxu@yyy.es', userId: 'vpI4tg6GMVPCCNLVOfxt3mICPH33', receipts: 1 },
+        { userEmail: 'sxxxxxxxxxx3@yyy.con', userId: 'in3J0i248OcWTgXix3Lbu2xLlfw2', receipts: 1 },
+        { userEmail: 'sxxxxxxxxxxi@yyy.com', userId: 'LzglgCwjiGc2WbdElyC6Krl245b2', receipts: 1 },
+        { userEmail: 'txxxxxxxxxxr@yyy.com', userId: 'uFOXBM39M6SRVA28D1q9MjYgfk32', receipts: 1 },
+        { userEmail: 'vxxxxxxxxxxo@yyy.com', userId: 'yZQxboeHHXV98CyKobv2pxBVQHU2', receipts: 1 },
+        {
+            userEmail: 'vxxxxxxxxxxk@yyy.com',
+            userId: 'sf8bwh7UkxZTD4XddfC0ri8ALt62',
+            receipts: 1
+        },
+        { userEmail: 'zxxxxxxxxxxb@yyy.com', userId: 'BqX4PsORvyfUNXC1YUEHDGBucvA2', receipts: 1 }
+    ],
+    usersWithReferralCode: 43,
+    referrals: [
+        { userEmail: 'vxxxxxxxxxx4@yyy.com', userId: 'ELxHiSZvYGeBJxEVMq1ZNwxO19i1', referrals: 5 },
+        { userEmail: 'axxxxxxxxxxo@yyy.com', userId: 'lzUL9oGCdDVfBfMu07YNvIzchHl2', referrals: 1 },
+        {
+            userEmail: 'cxxxxxxxxxxs@yyy.com',
+            userId: '3ayUIFRc4YSrchsoQNBIOFumOp72',
+            referrals: 1
+        },
+        {
+            userEmail: 'exxxxxxxxxx7@yyy.com',
+            userId: 'U0gJ8czJTgfamuXhK19xNoL74Xw2',
+            referrals: 1
+        },
+        { userEmail: 'exxxxxxxxxx8@yyy.com', userId: 'ktt5bc3LgiWH94KottjmM64SeL52', referrals: 1 },
+        {
+            userEmail: 'ixxxxxxxxxxo@yyy.com',
+            userId: 'WgFZihXtdeY7zgXII0NyPe8YLpB2',
+            referrals: 1
+        },
+        {
+            userEmail: 'pxxxxxxxxxx5@yyy.com',
+            userId: '3jDZZvPNjTbvE5BsHSEyfQdKjmW2',
+            referrals: 1
+        },
+        {
+            userEmail: 'sxxxxxxxxxx7@yyy.com',
+            userId: 'eT9leLc4LOTG3H1Z2IraQ5tZ7F02',
+            referrals: 1
+        }
+    ]
+};
